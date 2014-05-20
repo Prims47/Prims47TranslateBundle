4b014c724fb107f81ab150ed02d62590ea285c63
Ilan Benichou <ibenichou@ekino.com>
2014-05-20 17:17:28 +0200

    Fix dependancy services xml

--- DependencyInjection/Prims47TranslateExtension.php
+++ DependencyInjection/Prims47TranslateExtension.php
@@ -30,7 +30,5 @@ class Prims47TranslateExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.xml');
     }
 }
